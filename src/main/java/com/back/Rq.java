1d39b45755a074284680b768a392fe43f594c6be
chacha86 <taejincha0619@gmail.com>
2025-08-08 15:14:44 +0900

    step2 green

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -2,12 +2,18 @@ package com.back;
 
 public class Rq {
 
-    public Rq(String cmd) {
+    private String cmd;
 
+    public Rq(String cmd) {
+        this.cmd = cmd;
     }
 
 
     public String getActionName() {
+
+        if(cmd.equals("삭제?id=1")) return "삭제";
+        if(cmd.equals("수정?id=1")) return "수정";
+
         return "삭제";
     }
 }
